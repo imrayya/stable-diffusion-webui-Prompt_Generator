33683c21eaacbc9386f2ee73c711e42c5b890893
Imrayya <imrayya@gmail.com>
2023-01-11 17:55:02 +0100

    Doesn't run pip when already installed

--- install.py
+++ install.py
@@ -1,2 +1,5 @@
 import launch
-launch.run_pip("install --upgrade transformers", "Requirment of Prompt-Maker")
+if not launch.is_installed("transformers"):
+    launch.run_pip("install --upgrade transformers", "Requirement of Prompt-Maker")
+if not launch.is_installed("torch"):
+    launch.run_pip("install --upgrade torch", "Requirement of Prompt-Maker")
